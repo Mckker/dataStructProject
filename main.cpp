1f55fbc5a5a79fedbca2d33bff3ac24bc99880a0
Mack <mack@device2.lan>
2026-03-12 09:19:46 -0500

    Add outline

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "rlutil.h"
+ 
 using namespace std;
 
 
@@ -19,13 +19,11 @@ class Login{};
 class Admin : public User{};
 class Customer : public User{};
 int main() {
-    rlutil::cls();
-//    rlutil::hidecursor();
+
     int n = 1;
     while(n <= 100){
         std::cout << "Test" <<  n  <<" \n";
         n++;
     }
-    //rlutil::locate(1, 1);
     return 0;
 }
